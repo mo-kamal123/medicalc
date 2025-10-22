11551d0d15aaf1fdff4939e7fe1114bee2264787
mo-kamal123 <mk4086219@gmail.com>
2025-10-22 12:16:55 +0300

    implement main root and hero section and formating the code

---
+++ src/app/layout/root-layout.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import Navbar from "../../shared/layout/navbar";
+import Header from "../../shared/UI/header";
+
+const RootLayout = () => {
+  const loggedIn = useSelector((state) => state.auth.loggedIn);
+
+  return loggedIn ? (
+    <div className="bg-body min-h-svh flex flex-col">
+      <div className="w-[90%] m-auto pt-5 flex flex-col flex-1">
+        <Navbar />
+        <Header
+          heading={'Smart Self-Management Calculator Empowering Smarter Program Control'}
+          desc={'The Smart Self-Management Calculator is an intelligent tool designed to help healthcare and insurance'}
+        />
+        <div className="bgImg flex-1 flex">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to={'/auth'} replace />
+  );
+};
+
+export default RootLayout
